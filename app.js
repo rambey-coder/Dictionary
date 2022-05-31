7984c9b83c7fc86742aef853ce49b77ed59cf45f
Rambey <olayiwolaagboolaakanji@gmail.com>
2022-05-31 12:21:31 +0100

    search functionality started

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //  https://api.dictionaryapi.dev/api/v2/entries/en/
+
+const searchInput =  document.querySelector('#search'),
+      resultContent = document.querySelector('.result-output');
+
+
+searchInput.addEventListener('keyup', (e) => {
+    console.log(e.target.value);
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${e.target.value}`)
+        .then(res => console.log(res.data))
+})
