2ce5839474755645c2e8c3bfdd1dbed8d82e76dd
Rambey <olayiwolaagboolaakanji@gmail.com>
2022-05-31 14:11:00 +0100

    outputing value

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-//  https://api.dictionaryapi.dev/api/v2/entries/en/
+const btn = document.querySelector('.submit');
+const resultContent = document.querySelector('.result-output');
+const searchInput =  document.querySelector('#search');
 
-const searchInput =  document.querySelector('#search'),
-      resultContent = document.querySelector('.result-output');
 
 
-searchInput.addEventListener('keyup', (e) => {
-    console.log(e.target.value);
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${e.target.value}`)
-        .then(res => console.log(res.data))
+btn.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    fetch(` https://api.dictionaryapi.dev/api/v2/entries/en/${searchInput.value} `)
+        .then(res => res.json())
+        .then(data => {
+            // let output = '';
+            // setData(data)
+
+            resultContent.innerHTML = `
+            <div>
+                <h1>${data[0].word}</h1>
+                <div>
+                    <p>${data[0].phonetic}</p>
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                </div>
+            </div>
+            `;
+
+        })
+
+        // const setData = (results, output) => {
+        //     results.forEach(element => {
+        //         console.log(element);
+        //         resultContent.innerHTML = `
+                    // <div>
+                    //     <h1>${element.word}</h1>
+                    //     <div>
+                    //         <p>${element.phonetic}</p>
+                    //         <p>${element.meanings.antonyms}</p>
+                    //     </div>
+                    // </div>
+        //         `;
+        //     });
+        // }
 })
